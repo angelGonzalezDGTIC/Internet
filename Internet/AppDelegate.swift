a8fba78bec2788915c9122f441e01a619b09675b
Ángel González <jan.zelaznog@gmail.com>
2022-10-28 21:05:14 -0500

    Código de la sesión 1

--- Internet/AppDelegate.swift
+++ Internet/AppDelegate.swift
@@ -6,17 +6,56 @@
 //
 
 import UIKit
+import Network
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var internetStatus = false
+    var internetType = ""
+    
+    // 1. la aplicación ya se lanzó
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let monitor = NWPathMonitor()
+        // closure: Es un bloque de código que se ejecuta de maner asyncrona (funciones anónimas)
+        monitor.pathUpdateHandler = { path in
+            // path es un argumento que llega a la función
+            if path.status != .satisfied {
+                self.internetStatus = false
+            }
+            else {
+                self.internetStatus = true
+             // TODO: si hay internet, checar de que tipo
+                if path.usesInterfaceType(.wifi) {
+                    self.internetType = "WIFI"
+                }
+                else if path.usesInterfaceType(.cellular) {
+                    self.internetType = "CELL"
+                }
+            }
+        }
+        //
+        monitor.start(queue: DispatchQueue.global())
         return true
     }
-
+    // 2. El usuario se va a cambiar de app (o llegó una interrupción del s.o. ej. una llamada
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+    // 3. La app ya no está activa
+    func applicationDidEnterBackground(_ application: UIApplication) {
+ 
+    }
+    // 4. La app va a volver a primer plano
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+    // 5. La app ya está activa nuevamente
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Internet/ViewController.swift
+++ Internet/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import WebKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let ad = UIApplication.shared.delegate as! AppDelegate
+        if ad.internetStatus {
+            // TODO:
+            // 1. instanciar un objeto webview y colocarlo en la vista
+            let wv = WKWebView(frame:self.view.frame)
+            wv.allowsBackForwardNavigationGestures = true
+            self.view.addSubview(wv)
+            // 2. Obtener una página web y cargarla en el objeto webview
+            if let laUrl = URL(string: "http://janzelaznog.com") {
+                wv.load(URLRequest(url: laUrl))
+            }
+        }
+        else {
+            let ac = UIAlertController(title:"Error", message:"Lo sentimos, pero al parecer no hay conexión a Internet", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Enterado", style: .default)
+            ac.addAction(action)
+            self.present(ac, animated: true)
+        }
+    }
+    
 
 }
 
